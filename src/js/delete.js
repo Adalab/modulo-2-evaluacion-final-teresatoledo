d4410c4421ddc89500442d17b0e4e63a5dd8ee02
Teresa Toledo <laratoledoteresa@hotmail.com>
2024-01-22 10:47:03 +0100

    Add readme

--- src/js/delete.js
+++ src/js/delete.js
@@ -2,7 +2,6 @@ function handleRemove(event) {
 	//Vuelve la sección de favoritos a vacío, busca el id del padre de la papelera clicada, busca el index en el array de favoritos, con el splice lo elimina y vuelve a renderizar los favoritos.
 	favouriteSection.innerHTML = '';
 	const idFavouriteClicked = parseInt(event.currentTarget.parentNode.id);
-
 	const indexFav = favouriteSeries.findIndex(
 		(serie) => serie.mal_id === idFavouriteClicked
 	);
